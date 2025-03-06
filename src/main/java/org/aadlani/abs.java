a0b3ff78e055714bf2a8114fd500b48ad7d923fa
aadlani <aadlani9@gmail.com>
2025-03-06 12:50:40 +0100

    committt

--- src/main/java/org/aadlani/abs.java
+++ src/main/java/org/aadlani/abs.java
@@ -1,10 +1,12 @@
 package org.aadlani;
 
 
+import javax.swing.*;
+
 public class abs {
     public static int abs(int x) {
         if (x >= 0) {
-
+            Spring.out.print("ll")
             return x;
 
         } else {
