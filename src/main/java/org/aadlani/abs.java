9d18fc449968d98231468011e2ffdb85368d45f4
aadlani <aadlani9@gmail.com>
2025-03-06 12:47:50 +0100

    commit2

--- src/main/java/org/aadlani/abs.java
+++ src/main/java/org/aadlani/abs.java
@@ -14,4 +14,13 @@ public class abs {
         int number = -51;
         System.out.println(" :" + number + " " + abs(number));
     }
+    public static void main(String[] args) {
+        int number = -51;
+        System.out.println(" :" + number + " " + abs(number));
+    }
+    public static void main(String[] args) {
+        int number = -51;
+        System.out.println(" :" + number + " " + abs(number));
+    }
+
 }
