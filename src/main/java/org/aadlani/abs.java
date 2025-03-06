1b07d0561cbc52eaf193d34fad9336354358b71b
aadlani <aadlani9@gmail.com>
2025-03-06 12:47:01 +0100

    commit

---
+++ src/main/java/org/aadlani/abs.java
@@ -0,0 +1,17 @@
+package org.aadlani;
+
+
+public class abs {
+    public static int abs(int x) {
+        if (x >= 0) {
+            return x;
+        } else {
+            return -x;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = -51;
+        System.out.println(" :" + number + " " + abs(number));
+    }
+}
